d8b17c557b66e8c7a2ad92f316d30e8c8781faf7
RYUJEONGHUN <ryugh0210@naver.com>
2025-03-22 23:08:18 +0900

    UsernamePassword AuthenticationFilter && AuthenticationManager구현

---
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/jwt/LoginFilter.java
@@ -0,0 +1,60 @@
+package com.onboardingbackend.BlogProjectBackend.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onboardingbackend.BlogProjectBackend.dto.LoginRequestDTO;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager){
+        this.authenticationManager=authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        System.out.println("=== LoginFilter 진입 ===");
+        //클라이언트 요청에서 username, password 추출
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginRequestDTO loginDTO = null;
+        try {
+            loginDTO = objectMapper.readValue(request.getInputStream(), LoginRequestDTO.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String username = loginDTO.getUsername();
+        String password = loginDTO.getPassword();
+
+        System.out.println(username);
+
+
+        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
+        UsernamePasswordAuthenticationToken authToken= new UsernamePasswordAuthenticationToken(username,password,null);
+
+        //token에 담은 검증을 위한 AuthenticationManager로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
+}
