99f1a8d6d84cf81495bad9bcb71b7d5a8132b95e
RYUJEONGHUN <ryugh0210@naver.com>
2025-03-22 22:17:49 +0900

    spring security 기본세팅

---
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/controller/MainContoller.java
@@ -0,0 +1,15 @@
+package com.onboardingbackend.BlogProjectBackend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainContoller {
+
+    @GetMapping("/")
+    public String mainP(){
+        return "Main Controller";
+    }
+}
---
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/dto/JoinDTO.java
@@ -0,0 +1,15 @@
+package com.onboardingbackend.BlogProjectBackend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinDTO {
+
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+
+}
