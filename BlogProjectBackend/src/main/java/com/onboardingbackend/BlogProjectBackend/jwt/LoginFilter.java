67f56560c6475461eb4b3d60b9e484cc4bc0fd36
RYUJEONGHUN <ryugh0210@naver.com>
2025-03-23 02:06:30 +0900

    로그인 성공 jwt 발급

--- BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/jwt/LoginFilter.java
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/jwt/LoginFilter.java
@@ -1,6 +1,7 @@
 package com.onboardingbackend.BlogProjectBackend.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onboardingbackend.BlogProjectBackend.dto.CustomerUserDetails;
 import com.onboardingbackend.BlogProjectBackend.dto.LoginRequestDTO;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServlet;
@@ -10,16 +11,20 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
 
-    public LoginFilter(AuthenticationManager authenticationManager){
+    public LoginFilter(AuthenticationManager authenticationManager,JWTUtil jwtUtil){
         this.authenticationManager=authenticationManager;
+        this.jwtUtil=jwtUtil;
     }
 
     @Override
@@ -50,11 +55,23 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-        System.out.println("success");
+        CustomerUserDetails customerUserDetails=(CustomerUserDetails) authentication.getPrincipal();
+
+        String username=customerUserDetails.getUsername();
+
+        Collection<? extends GrantedAuthority> authorities=authentication.getAuthorities();
+        Iterator<? extends  GrantedAuthority> iterator=authorities.iterator();
+        GrantedAuthority auth= iterator.next();
+
+        String role=auth.getAuthority();
+
+        String token=jwtUtil.createJwt(username,role,60*60*10L);
+
+        response.addHeader("Authorization","Bearer "+ token);
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-        System.out.println("fail");
+        response.setStatus(401);
     }
 }
