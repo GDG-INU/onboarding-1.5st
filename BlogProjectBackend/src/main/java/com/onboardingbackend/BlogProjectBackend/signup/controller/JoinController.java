d72f692d13cba4bc5a3ce7145eeeaee8d39ca269
RYUJEONGHUN <ryugh0210@naver.com>
2025-03-24 00:53:34 +0900

    signup 상위폴더로 이동

--- BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/signup/controller/JoinController.java
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/signup/controller/JoinController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j   //System.out.println은 느림
 @RestController  //RestController로 변경
