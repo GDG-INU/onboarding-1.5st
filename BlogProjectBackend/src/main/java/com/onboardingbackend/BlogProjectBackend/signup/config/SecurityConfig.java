1b270456694a03b2311b10ec6086e6f2f688c4bd
RYUJEONGHUN <ryugh0210@naver.com>
2025-03-27 18:34:41 +0900

    설정 파일

--- BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/signup/config/SecurityConfig.java
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/signup/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.onboardingbackend.BlogProjectBackend.config;
+package com.onboardingbackend.BlogProjectBackend.signup.config;
 
 import com.onboardingbackend.BlogProjectBackend.signup.jwt.JWTFilter;
 import com.onboardingbackend.BlogProjectBackend.signup.jwt.JWTUtil;
@@ -48,6 +48,7 @@ public class SecurityConfig {
                 .csrf((auth) ->auth.disable())
                 .formLogin((auth) ->auth.disable())
                 .httpBasic((auth) ->auth.disable()); //체이닝 적용
+
 
         http
                 .authorizeHttpRequests((auth)->auth
