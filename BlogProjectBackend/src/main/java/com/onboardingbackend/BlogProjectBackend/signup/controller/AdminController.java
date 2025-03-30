f8f04e87fcd06163a7972f9d254ad5b337c295e2
RYUJEONGHUN <ryugh0210@naver.com>
2025-03-31 01:20:48 +0900

    Response를 string 아닌 Json으로 반환하도록 수정

--- BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/signup/controller/AdminController.java
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/signup/controller/AdminController.java
@@ -1,9 +1,13 @@
 package com.onboardingbackend.BlogProjectBackend.signup.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController//RestController 적용
@@ -11,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
     @GetMapping("/admin")
-    public String adminP(){
-        return "Admin Controller";
+    public ResponseEntity<Map<String, String>> adminP(){
+        Map<String, String> message = new HashMap<>();
+        message.put("message", "Admin_Controll");
+        return ResponseEntity.ok(message);
     }
 }
---
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/signup/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.onboardingbackend.BlogProjectBackend.signup.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException e) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(UsernameNotFoundException e) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(RuntimeException e) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
