d4dce8775a30045148b805ea7b8475508ab18f8f
dmswl0103 <leg050103@gmail.com>
2025-04-03 00:00:09 +0900

    [FEAT] 댓글 삭제

--- BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/comment/controller/CommentController.java
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/comment/controller/CommentController.java
@@ -3,9 +3,11 @@ package com.onboardingbackend.BlogProjectBackend.comment.controller;
 import com.onboardingbackend.BlogProjectBackend.comment.dto.req.CommentRequestDto;
 import com.onboardingbackend.BlogProjectBackend.comment.dto.res.CommentResponseDto;
 import com.onboardingbackend.BlogProjectBackend.comment.service.CommentService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +30,13 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createComment);
     }
 
-
+    // 댓글 삭제
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Boolean> deletecomment(
+            @PathVariable Integer id,
+            @AuthenticationPrincipal UserDetails userDetails){
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.delete(id, userDetails));
+    }
 
 
 
