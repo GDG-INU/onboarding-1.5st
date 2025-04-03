f5b0b0b8dbfac6463ebe5c31b919e5d11698072f
RYUJEONGHUN <ryugh0210@naver.com>
2025-04-03 11:14:12 +0900

    댓글기능구현

--- BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/comment/repository/CommentRepository.java
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/comment/repository/CommentRepository.java
@@ -3,6 +3,9 @@ package com.onboardingbackend.BlogProjectBackend.comment.repository;
 import com.onboardingbackend.BlogProjectBackend.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
+    List<Comment> findByBoard_IdAndParentCommentIsNull(Integer boardId);
 }
--- BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/comment/service/CommentService.java
+++ BlogProjectBackend/src/main/java/com/onboardingbackend/BlogProjectBackend/comment/service/CommentService.java
@@ -8,6 +8,7 @@ import com.onboardingbackend.BlogProjectBackend.comment.entity.Comment;
 import com.onboardingbackend.BlogProjectBackend.comment.repository.CommentRepository;
 import com.onboardingbackend.BlogProjectBackend.signup.entity.UserEntity;
 import com.onboardingbackend.BlogProjectBackend.signup.repository.UserRepository;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Transactional
 @RequiredArgsConstructor
@@ -32,23 +35,53 @@ public class CommentService {
         UserEntity user = userRepository.findByEmail(userDetails.getUsername())
                 .orElseThrow(()-> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다."));
 
+        Comment parent = null;
+        if (commentRequestDto.getParentCommentId() != null) {
+            parent = commentRepository.findById(commentRequestDto.getParentCommentId())
+                    .orElseThrow(() -> new IllegalArgumentException("부모 댓글 없음"));
+        }
+
         Comment comment = new Comment();
         comment.setContent(commentRequestDto.getContent());
+        comment.setUpdatedAt(LocalDateTime.now());
         comment.setCreatedAt(LocalDateTime.now());
         comment.setLikeCount(0);
         comment.setBoard(board);
         comment.setUser(user);
+        comment.setParentComment(parent);
 
         Comment savedComment = commentRepository.save(comment);
-        return new CommentResponseDto(savedComment);
+
+        return CommentResponseDto.of(savedComment);
     }
 
     // 댓글 삭제
-    public Boolean delete(Integer id, UserDetails userDetails){
+    public Boolean delete(Integer id){
         Comment comment = commentRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("댓글을 찾을 수 없습니다."));
         commentRepository.delete(comment);
         return true;
     }
+
+    //댓글 수정
+    public CommentResponseDto update(Integer commentId, CommentRequestDto commentRequestDto) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+        comment.updateContent(commentRequestDto.getContent());
+
+        return CommentResponseDto.of(comment);
+    }
+
+    /**
+     * 댓글 좋아요
+     */
+    @Transactional
+    public CommentResponseDto likeComment(Integer commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+        comment.increaseLike();
+        return CommentResponseDto.of(comment);
+    }
+
 
 }
